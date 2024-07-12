0de2b7bda76666d031fa55472962fa8e2421678e
vikas <vikas.kumar@netscapelabs.com>
2024-07-12 16:15:37 +0530

    done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({ toggleSidebar, toggleTheme, isDarkMode,isSidebarOpen }) => {
   };
 
   return (
-    <div className={`flex justify-between items-center h-16 w-full z-10 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-200 text-gray-900'} shadow-lg`}>
+    <div className={`flex justify-between items-center h-16 fixed w-full ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-200 text-gray-900'} shadow-lg`}>
       {!isSidebarOpen && <button onClick={toggleSidebar} className="px-4 focus:outline-none">
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
