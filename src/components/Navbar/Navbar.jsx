827f291b33e81a6605681ed268797540be779d20
vikas <vikas.kumar@netscapelabs.com>
2024-07-12 17:57:49 +0530

    done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Navbar = ({ toggleSidebar, toggleTheme, isDarkMode, isSidebarOpen }) => {
-  console.log("isSidebarOpen", isSidebarOpen);
+  const [navbarWidth, setNavbarWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setNavbarWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("Navbar width:", navbarWidth);
+  }, [navbarWidth]);
+
   const handleThemeToggle = () => {
     toggleTheme(!isDarkMode); // Toggle dark mode state
   };
 
   return (
-    <div className={`flex justify-between items-center h-16 fixed w-full ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-200 text-gray-900'} shadow-lg z-50`}>
-      {!isSidebarOpen && (
-        <button onClick={toggleSidebar} className="px-4 focus:outline-none">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
-      )}
-      <h1 className="text-lg font-bold">Admin Panel</h1>
-      <div className={`px-4 ${isSidebarOpen?' mr-[15.5%]':'mr-[0]'}`}>
+    <div className={`flex justify-between items-center z-0 h-16 fixed top-0 left-0 right-0 z-50 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-200 text-gray-900'} shadow-lg`}>
+      <div className="flex items-center space-x-4 px-4">
+        {!isSidebarOpen && (
+          <button onClick={toggleSidebar} className="focus:outline-none">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        )}
+      </div>
+      <div className="px-4 ml-auto">
         <button onClick={handleThemeToggle} className="focus:outline-none">
           {isDarkMode ? (
             <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
